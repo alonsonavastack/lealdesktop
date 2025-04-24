9157a60f92c0fd8d425cdc1cc7620b17ac4211d5
Alonso Nava <alonso.nava086@gmail.com>
2025-04-24 15:57:23 -0600

    version 1

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,18 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { FirebaseService } from '../../services/firebase.service';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterLink, RouterOutlet],
+  standalone: true,
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  private router = inject(Router);
+  private firebaseSvc = inject(FirebaseService);
 
   menuOpen = false;
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
   }
-  
+
+  logout() {
+    localStorage.removeItem('user');
+    this.router.navigate(['/auth']);
+  }
 }
